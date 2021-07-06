78b8add21e0c45e6981db6c852b0b6f486325958
Sergey Vishnevsky <serejich@live.com>
2021-07-06 14:17:22 +0300

    Init

---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils';
+
+const Parent = {
+  mounted() {
+    console.log('Parent has mounted');
+  },
+  template: `
+    <div>
+      <p>I am a parent</p>
+      <slot />
+    </div>
+  `,
+};
+
+const Child = {
+  mounted() {
+    console.log('Child has mounted');
+  },
+  render() {
+    return '<p>I am a child</p>';
+  },
+};
+
+describe('Parent', () => {
+  it("doesn't call Child lifecycle hooks", () => {
+    mount(Parent, {
+      slots: {
+        default: Child,
+      },
+    });
+  });
+
+  it('calls Child lifecycle hooks', () => {
+    mount({
+      components: { Parent, Child },
+      template: `
+        <Parent>
+          <Child />
+        </Parent>
+      `,
+    });
+  });
+});
